4624b5d5751083dff5e587a37e1acb83bc07614c
Büşra Talih <busra.talihhh@gmail.com>
2023-01-12 22:08:47 +0300

    12.01.2023

---
+++ src/test/java/runner/SeleniumGrid.java
@@ -0,0 +1,34 @@
+package runner;
+
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class SeleniumGrid {
+
+    static WebDriver driver;
+
+    public static void main(String[] args) throws MalformedURLException {
+
+        DesiredCapabilities cap=new DesiredCapabilities();
+        cap.setPlatform(Platform.WIN11);
+        cap.setBrowserName("firefox");
+
+        driver=new RemoteWebDriver(new URL("http://192.168.1.105:4444"), cap);
+        driver.manage().window().maximize();
+        driver.get("https://www.google.com/");
+
+        driver.close();
+
+
+
+
+
+    }
+}
